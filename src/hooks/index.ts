de4d0850c2adfafe22ca4c2167e6f7b51ab1eb7b
Lien Hung Trinh <thuyhung2112@yahoo.com>
2026-09-09 15:06:24 +0700

    cleanup pages and assets

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -5,5 +5,4 @@ export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
 export const useAppStore: () => AppStore = useStore;
 
-export * from "./useMediaQuery";
 export * from "./useScrollDirection";
--- src/shared/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./FAQSection";
-export * from "./JoinLoversSection";
-export * from "./JoinRitualSection";
-export * from "./OrganicVeganSection";
-export * from "./SlowReleaseSection";
-export * from "./TrackedSealedSection";
